efe32f259773ae7636100e2919b0a3fd1328282a
primeinc <4395149+primeinc@users.noreply.github.com>
2024-09-21 10:55:18 -0400

    Refactored error component to ensure `errObj` is always an object, preventing potential runtime errors. Updated to display default error code 500 if undefined. Integrated theme configuration, allowing dynamic theme assignment and improving flexibility in theme management. Minor logging adjustments for a cleaner development experience.
    This addresses error handling reliability and theme configuration flexibility.

--- error.vue
+++ error.vue
@@ -1,10 +1,10 @@
 <template>
-  <theme-wrapper theme="ocean">
+  <theme-wrapper :theme="theme">
     <v-app>
       <div>
         <main-container invert>
           <Error
-            :error-code="errObj.statusCode"
+            :error-code="errObj?.statusCode || 500"
             :text="$t('common.404')"
           />
         </main-container>
@@ -18,6 +18,7 @@ import brand from './assets/text/brand';
 import ThemeWrapper from './components/ThemeWrapper';
 import MainContainer from './components/MainContainer';
 import Error from './components/Error';
+import theme from '../config/theme'; // Import the theme configuration
 import { useHead, useError, defineNuxtComponent } from '#app';
 
 export default defineNuxtComponent({
@@ -28,14 +29,21 @@ export default defineNuxtComponent({
   },
   setup() {
     const error = useError();
-    const errObj = error._object.error;
-    // eslint-disable-next-line no-console
-    console.log(errObj); // Only log in development
+    const errObj = error.value || {}; // Ensure errObj is always an object
+    if (process.client && process.env.NODE_ENV === 'development') {
+      // eslint-disable-next-line no-console
+      // console.log(errObj);
+    }
     useHead({
       title: errObj.statusCode === 404
         ? brand.agency.name + ' - Not Found'
         : brand.agency.name + ' - An error occurred',
     });
+
+    return {
+      theme, // Return the theme to use in the template
+      errObj,
+    };
   },
 });
 </script>
